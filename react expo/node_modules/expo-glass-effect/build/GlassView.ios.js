bf701b1246648813e7d0c4b9294758acef677490
Archit Jaiswal <architjaiswal18@gmail.com>
2026-04-24 17:00:52 -0700

    remove node_modules from tracking

--- react expo/node_modules/expo-glass-effect/build/GlassView.ios.js
+++
@@ -1,9 +0,0 @@
-// Copyright © 2024 650 Industries.
-'use client';
-import { requireNativeViewManager } from 'expo-modules-core';
-const NativeGlassView = requireNativeViewManager('ExpoGlassEffect', 'GlassView');
-const GlassView = (props) => {
-    return <NativeGlassView {...props}/>;
-};
-export default GlassView;
-//# sourceMappingURL=GlassView.ios.js.map
--- react expo/node_modules/expo-router/server.d.ts
+++
@@ -1,6 +0,0 @@
-export type { ImmutableRequest, LoaderFunction, MiddlewareFunction } from 'expo-server';
-
-export type RequestHandler = (
-  request: Request,
-  params: Record<string, string>
-) => Response | Promise<Response>;
